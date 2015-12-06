63a4b99ee1fc341709916647847b4af4f518fca2
Dayn Brandon Carreto Bonilla <damcb164e9@gmail.com>
2015-12-06 16:10:59 -0600

    videojuegos1
    a

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ document.addEventListener("deviceready",function(){
 	$("#consulta1").on("tap", function (){
 		$.ajax({
 			type:"POST", 
-			url:"http://192.168.1.68/videojuegos/buscarTodos.php",
+			url:"http://192.168.1.71/videojuegos/buscarTodos.php",
 			error: function(){
 			 alert ('No se Puedo Conectar  a la Base de Datos');
 			}, 
@@ -15,7 +15,7 @@ document.addEventListener("deviceready",function(){
 		
 			for (var x= 0; x<datosjuegos.juegos.length; x++)
 			{
-				$('#todo').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
+				$('#todo').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.71/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#Pagina1',{
 				transition: 'pop'
@@ -30,7 +30,7 @@ document.addEventListener("deviceready",function(){
 		$.ajax({
 			type:"POST",
 			 
-			url:"http://192.168.1.68/videojuegos/buscarGenero.php",
+			url:"http://192.171.1.71/videojuegos/buscarGenero.php",
 			data: "genero=" + $('#txtgenero').val(), 
 			error: function(){
 			 alert ('No se Puedo Conectar  a la Base de Datos');
@@ -42,7 +42,7 @@ document.addEventListener("deviceready",function(){
 		
 			for (var x= 0; x<datosgenero.genero.length; x++)
 			{
-				$('#contenidogen').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosgenero.genero[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosgenero.genero[x].nombre_videojuego +' </h1><h2> '+datosgenero.genero[x].genero_videojuego+' </h2></div></div><div><h2> '+datosgenero.genero[x].clasificacion_videojuego+' </h2><h2> '+datosgenero.genero[x].consola_videojuego+' </h2></div></div>');
+				$('#contenidogen').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.71/videojuegos/recursos/imagenes/fotos/'+datosgenero.genero[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosgenero.genero[x].nombre_videojuego +' </h1><h2> '+datosgenero.genero[x].genero_videojuego+' </h2></div></div><div><h2> '+datosgenero.genero[x].clasificacion_videojuego+' </h2><h2> '+datosgenero.genero[x].consola_videojuego+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#genero',{
 				transition: 'pop'
@@ -56,7 +56,7 @@ document.addEventListener("deviceready",function(){
 		$("#btn_con").on("tap", function (){
 		$.ajax({
 			type:"POST", 
-			url:"http://192.168.1.68/videojuegos/buscarConsola.php",
+			url:"http://192.168.1.71/videojuegos/buscarConsola.php",
 			data: "consola=" + $('#txtconsola').val(),
 			error: function(){
 			 alert ('No se Puedo Conectar  a la Base de Datos');
@@ -68,7 +68,7 @@ document.addEventListener("deviceready",function(){
 		
 			for (var x= 0; x<datosconsola.consola.length; x++)
 			{
-				$('#contenidocon').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosconsola.consola[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosconsola.consola[x].nombre_videojuego +' </h1><h2> '+datosconsola.consola[x].genero_videojuego+' </h2></div></div><div><h2> '+datosconsola.consola[x].clasificacion_videojuego+' </h2><h2> '+datosconsola.consola[x].consola_videojuego+' </h2></div></div>');
+				$('#contenidocon').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.71/videojuegos/recursos/imagenes/fotos/'+datosconsola.consola[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosconsola.consola[x].nombre_videojuego +' </h1><h2> '+datosconsola.consola[x].genero_videojuego+' </h2></div></div><div><h2> '+datosconsola.consola[x].clasificacion_videojuego+' </h2><h2> '+datosconsola.consola[x].consola_videojuego+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#consola',{
 				transition: 'pop'
